180896b08acf38fc598d5d3750b9a13fb5a2fece
jpetersen6202 <33636781+jpetersen6202@users.noreply.github.com>
2020-02-28 17:29:53 -0600

    added wall things

---
+++ Assets/Scripts/apple/UnderWater.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnderWater : MonoBehaviour
+{
+    private Color normalColor;
+    private Color underwaterColor;
+    public int tester;
+
+    // Use this for initialization
+    void Start()
+    {
+        tester = 0;
+        normalColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        underwaterColor = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+    }
+
+    void SetNormal()
+    {
+        RenderSettings.fogColor = normalColor;
+        RenderSettings.fogDensity = 0.01f;
+        RenderSettings.fog = false;
+
+    }
+
+    void SetUnderwater()
+    {
+        RenderSettings.fogColor = underwaterColor;
+        RenderSettings.fogDensity = 1f;
+        RenderSettings.fog = true;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("water"))
+        {
+            tester = 1;
+            SetUnderwater();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("water"))
+        {
+            tester = 0;
+            SetNormal();
+        }
+    }
+}
