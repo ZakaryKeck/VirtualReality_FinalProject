f50275ea30f688ed8df588829349ddf4c5550f5a
Kameron Heyen <kameronh7@gmail.com>
2020-04-20 15:56:28 -0500

    add squirt to master

--- Assets/Scripts/WaterLevelController.cs
+++ Assets/Scripts/WaterLevelController.cs
@@ -12,30 +12,52 @@ public class WaterLevelController : MonoBehaviour
     public Target target;
     public GameObject water;
     public Cap tubeCap;
-    public bool full = false;
+
+    public GameObject Sparkles;
+    public AudioSource CompleteSound;
 
     private int waterLevel = 1;
+    private bool full = false;
+    private bool playedSounds = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (!tubeCap.isFull() && target.getActive())
         {
-            
-            UnityEngine.Debug.Log("Collide Detect");
             Transform waterTrans = water.GetComponent<Transform>();
             waterTrans.position += new Vector3(0, .001f, 0);
         }
-        else
+        if (tubeCap.isFull())
         {
             this.full = true;
+            if (this.full && !this.playedSounds)
+            {
+                this.playedSounds = true;
+                this.complete();
+            }
+            
         }
+    }
+
+    public bool IsFull()
+    {
+        return this.full;
+    }
+
+    private void complete()
+    {
+        GameObject sparkles = Instantiate(Sparkles, transform.position, transform.rotation);
+        sparkles.transform.localScale = new Vector3(.05f, .05f, .05f);
+        sparkles.GetComponent<ParticleSystem>().Play();
+        Destroy(sparkles, 1);
+        CompleteSound.Play();
+    }
+
+    public void reset()
+    {
+        this.full = false;
+        this.playedSounds = false;
+        // reset position of water tube
     }
 }
 
