a206d52b2a0b6dfd519f4c7ae8fa41e1c6ffc407
Kameron Heyen <kameronh7@gmail.com>
2020-05-05 12:20:18 -0500

    Create everything to control the cotton candy pour

---
+++ Assets/Scripts/cccontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class cccontroller : MonoBehaviour
+{
+    public GameObject blue;
+    public GameObject green;
+    public GameObject pink;
+
+    public GameObject blueSugar;
+    public GameObject greenSugar;
+    public GameObject pinkSugar;
+
+    public GameObject disk;
+
+    public Material blueMat;
+    public Material greenMat;
+    public Material pinkMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
